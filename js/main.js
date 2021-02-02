c819e784cb77993aab382c62d8f93076b7a35c41
Адель <selly.lady.zakirova@gmail.com>
2021-02-02 23:49:55 +0500

    Нужно больше функций. Доработка

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Функция, возвращающая случайное целое положительное число из переданного диапазона включительно
-const getRandomNumber = function (min, max) {
+const getRandomNumber =  (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
   if (min >= 0 && min <= max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return -1;
+  throw new Error(INVALID_ARGUMENT);
 };
 
 getRandomNumber(1, 9);
@@ -16,13 +16,13 @@ getRandomNumber(1, 9);
 
 
 // Функция, возвращающая случайное положительное число с заданным количеством цифр после запятой из переданного диапазона включительно
-const getRandomArbitrary = function (min, max, digits) {
+const getRandomArbitrary = (min, max, digits) => {
   // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
   if (min >= 0 && min <= max) {
   // метод toFixed оставляет у полученного случайного числа заданное количество цифр после запятой
     return (Math.random() * (max - min) + min).toFixed(digits);
   }
-  return -1;
+  throw new Error(INVALID_ARGUMENT);
 };
 
 getRandomArbitrary(1.1, 6.4, 2);
