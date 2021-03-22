8bb170b0c0de7650b59a3c1220379d3ef0c35234
Адель <selly.lady.zakirova@gmail.com>
2021-03-22 10:37:07 +0500

    Фото и сборка

--- source/js/avatar.js
+++ source/js/avatar.js
@@ -1,25 +1,24 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const PREVIEW_SIZE = 70;
 const DEFAULT_AVATAR_SRC = 'img/muffin-grey.svg';
-const DEFAULT_PHOTO_SRC = 'img/no-image.svg';
 
 const userUploader = document.querySelector('.ad-form__field input[type=file]');
 const userPreview = document.querySelector('.ad-form-header__preview img');
 
 const photoContainer = document.querySelector('.ad-form__photo');
 
-const houstingUploader = document.querySelector('.ad-form__upload input[type=file]');
-const houstingPreview = document.createElement('img');
+const photoUploader = document.querySelector('.ad-form__upload input[type=file]');
+const photoPreview = document.createElement('img');
 
 const setPreviewFiles = (selectedFile, preview) => {
   return (evt) => {
     evt.preventDefault();
     const file = selectedFile.files[0];
     const fileName = file.name.toLowerCase();
-    const matches = FILE_TYPES.some((fileType) => {
+    const photoFile = FILE_TYPES.some((fileType) => {
       return fileName.endsWith(fileType);
     });
-    if (matches) {
+    if (photoFile) {
       const reader = new FileReader();
       reader.addEventListener('load', () => {
         preview.src = reader.result;
@@ -31,15 +30,15 @@ const setPreviewFiles = (selectedFile, preview) => {
 
 const initPhotoUploaders = () => {
   userUploader.addEventListener('change', setPreviewFiles(userUploader, userPreview));
-  houstingUploader.addEventListener('change', setPreviewFiles(houstingUploader, houstingPreview));
-  houstingPreview.width = PREVIEW_SIZE;
-  houstingPreview.height = PREVIEW_SIZE;
-  photoContainer.append(houstingPreview);
+  photoUploader.addEventListener('change', setPreviewFiles(photoUploader, photoPreview));
+  photoPreview.width = PREVIEW_SIZE;
+  photoPreview.height = PREVIEW_SIZE;
+  photoContainer.append(photoPreview);
 };
 
 const resetPreview = () => {
   userPreview.src = DEFAULT_AVATAR_SRC;
-  houstingPreview.src = DEFAULT_PHOTO_SRC;
+  photoPreview.removeAttribute('src');
 };
 
 export { initPhotoUploaders, resetPreview };
